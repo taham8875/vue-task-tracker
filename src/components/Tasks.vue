65abf714dc7678bbda5577e94282fe581a3dd6d8
taham8875 <taha.m8875@gmail.com>
2024-01-01 22:41:26 +0200

    feat: Introduce Tasks component
    - Pass task data as props to the Task component and render it
    - Emit delete-task and toggle-reminder events from Task component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Task from './Task.vue';
+import type { TasksProps } from '../../index.d';
+
+const props = defineProps({
+    tasks: {
+        type: Array as () => TasksProps,
+        required: true
+    }
+});
+
+const emits = defineEmits(['delete-task', 'toggle-reminder']);
+</script>
+
+<template>
+    <div v-for="task in tasks">
+        <Task :key="task.id" :task="task" @delete-task="$emit('delete-task', task.id)"
+            @toggle-reminder="$emit('toggle-reminder', task.id)" />
+    </div>
+</template>
