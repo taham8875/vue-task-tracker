d4148fa18eef44772e69be827dd786bf07e9a707
taham8875 <taha.m8875@gmail.com>
2024-01-01 22:46:21 +0200

    feat: integrate Header and Footer components in App.vue
    - Import Header and Footer components
    - Use ref to manage the state of showAddTaskForm
    - Implement toggleAddTaskForm function to control the visibility of the task creation form
    - Include Header with toggle functionality and Footer in the main template
    - Apply styling to the #app element for improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import { ref } from 'vue';
+
+const showAddTaskForm = ref(false);
+
+const toggleAddTaskForm = () => {
+    showAddTaskForm.value = !showAddTaskForm.value;
+}
 </script>
 
 <template>
+    <Header @toggle-add-task-form="toggleAddTaskForm" :showAddTaskForm="showAddTaskForm" />
+    <router-view :showAddTaskForm="showAddTaskForm"></router-view>
+    <Footer />
 </template>
+
+
+<style>
+#app {
+    @apply max-w-xl mx-auto my-3 px-8 py-5 border border-gray-200 rounded;
+}
+</style>
