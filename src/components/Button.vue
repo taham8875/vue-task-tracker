d06309166446ef06a7583bca55bdb51f17bfe6b3
taham8875 <taha.m8875@gmail.com>
2024-01-01 16:35:12 +0200

    feat: Add dynamic variant button component
    - Introduce a new button component with dynamic background colors based on the `variant` prop.
    - Variants include `primary`, `create`, and `delete`.

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+    variant: {
+        type: String,
+        default: 'primary'
+    }
+})
+
+const colorClasses = computed(() => {
+    if (props.variant === 'primary') {
+        return 'bg-black hover:bg-gray-900'
+    } else if (props.variant === 'create') {
+        return 'bg-green-600 hover:bg-green-700'
+    } else if (props.variant === 'delete') {
+        return 'bg-red-600 hover:bg-red-700'
+    }
+})
+
+</script>
+
+<template>
+    <button class="btn rounded py-2 px-4 transition-colors text-white" :class="colorClasses">
+        <slot></slot>
+    </button>
+</template>
