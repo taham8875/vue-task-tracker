419dae588e28385e29a76d20c82887573e50939d
taham8875 <taha.m8875@gmail.com>
2024-01-01 22:43:16 +0200

    feat: Introduce About View - Display version 1.0.0 in the About page - Add a hyperlink to the GitHub profile - Include a router-link to navigate back to the home page

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="my-5">
+        <h3>Version 1.0.0</h3>
+        <a href="https://github.com/taham8875" class="underline text-green-700 font-mono">https://github.com/taham8875</a>
+        <router-link to="/"></router-link>
+    </div>
+</template>
