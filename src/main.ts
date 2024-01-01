945bf27062152c62c54bc66f756cc00a1eb88741
taham8875 <taha.m8875@gmail.com>
2024-01-01 22:37:30 +0200

    feat: integrate Vue Router
    - Import Vue Router in main.ts
    - Create a router instance with routes for Home and About components
    - Use the router instance with the Vue app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import router from './router/index'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
