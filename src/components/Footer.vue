d69250bc68266d2be66db1d14015d9f133bd9042
taham8875 <taha.m8875@gmail.com>
2024-01-01 22:35:37 +0200

    feat: Implement Footer component
    - Use Vue Router to dynamically set the footer link based on the current route
    - Implement a watchEffect to update the link when the route changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { watchEffect, ref } from 'vue';
+import { useRouter } from 'vue-router';
+const currentYear = new Date().getFullYear();
+
+const router = useRouter();
+
+const footerLink = ref({
+    name: '',
+    path: ''
+});
+
+watchEffect(() => {
+    if (router.currentRoute.value.path === '/') {
+        footerLink.value.name = 'About';
+        footerLink.value.path = '/about';
+    } else {
+        footerLink.value.name = 'Back to Home';
+        footerLink.value.path = '/';
+    }
+});
+
+
+</script>
+
+<template>
+    <footer class="text-center text-gray-500 text-sm mt-4">
+        &copy; {{ currentYear }} Task Tracker - <router-link :to="footerLink.path" class="underline text-green-700"> {{
+            footerLink.name }}</router-link>
+    </footer>
+</template>
